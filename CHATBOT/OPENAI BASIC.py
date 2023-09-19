23da66888cb935f83119e7d6d9e49218c4728069
Nishaan Padanthaya <145466368+NishaanPadanthaya@users.noreply.github.com>
2023-09-19 20:56:04 +0530

    Update OPENAI BASIC.py

--- CHATBOT/OPENAI BASIC.py
+++ CHATBOT/OPENAI BASIC.py
@@ -5,6 +5,8 @@ import openai
 
 openai.api_key = 'yourapikey'
 
+#this api key can be found when you create an openai account and access your api key
+
 print("welcome to chatgpt")
 
 prompt = input("enter your request : ")
